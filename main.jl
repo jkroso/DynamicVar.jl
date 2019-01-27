5e67497253f18dd143b279fedba6aa8b7b4eeb80
Jake Rosoman <jkroso@gmail.com>
2019-01-27 18:54:25 +1100

    switch to using Refs
    Because Tasks no longer support `.parent` so it's no longer possible to implement the anything that's thread safe

--- main.jl
+++ main.jl
@@ -1,101 +1,35 @@
-@require "github.com/jkroso/Prospects.jl" need assoc
-
-struct DynamicVar
-  name::Symbol
-end
-
-# Creates a new DynamicVar
 dynamic_eq(def) = begin
   name, val = map(esc, def.args)
-  quote
-    const $name = DynamicVar($(QuoteNode(def.args[1])))
-    bind($name, $val, current_task())
-  end
-end
-
-# Alters the value of a dynamic variables within the scope of an expression
-dynamic_let(ex) = begin
-  vars = Meta.isexpr(ex.args[1], :block) ? ex.args[1].args : [ex.args[1]]
-  quote
-    t = @task $(esc(ex.args[2]))
-    $([:(bind($(esc(b.args[1])), $(esc(b.args[2])), t)) for b in vars]...)
-    schedule(t)
-    fetch(t)
-  end
+  :(const $name = Ref{Any}($val))
 end
 
 dynamic_let!(ex) = begin
   vars = Meta.isexpr(ex.args[1], :block) ? ex.args[1].args : [ex.args[1]]
   quote
-    t = current_task()
-    old = getscope(t)
-    $([:(bind($(esc(b.args[1])), $(esc(b.args[2])), t)) for b in vars]...)
+    old = [$([:($(esc(x.args[1]))[]) for x in vars]...)]
+    $([:($(esc(b.args[1]))[] = $(esc(b.args[2]))) for b in vars]...)
     result = $(esc(ex.args[2]))
-    setscope(t, old)
+    $([:($(esc(b.args[1]))[] = old[$i]) for (i,b) in enumerate(vars)]...)
     result
   end
 end
 
-setscope(t::Task, scope) = Base.get_task_tls(t)[getscope] = scope
-
 """
-Create new or alter existing dynamic variables within a limited scope
+Create new or alter existing Refs within a limited scope
 
-To create a new dynamic variables write `@dynamic a = 1`
+To create a new Ref write `@dynamic! a = 1` or `const a = Ref(1)`
 To alter it within a limited scope write:
 
 ```
-show_a() = @show need(a)
-@dynamic let a = 2
-  show_a()
+@show a[]
+@dynamic! let a = 2
+  @show a[]
 end
-show_a()
+@show a[]
 ```
-"""
-macro dynamic(def)
-  Meta.isexpr(def, :(=)) && return dynamic_eq(def)
-  Meta.isexpr(def, :let) && return dynamic_let(def)
-  error("Unsupported @dynamic expression")
-end
-
-"""
-Like dynamic but unsafe when the body of the let contains async code
 """
 macro dynamic!(def)
   Meta.isexpr(def, :(=)) && return dynamic_eq(def)
   Meta.isexpr(def, :let) && return dynamic_let!(def)
   error("Unsupported @dynamic! expression")
 end
-
-need(var::DynamicVar) = begin
-  value = getvalue(var, Base.secret_table_token, current_task())
-  @assert value !== Base.secret_table_token "No value defined for $var"
-  value
-end
-
-parent(task::Task) = task.parent
-isroot(task::Task) = task.parent == task
-const empty_scope = Base.ImmutableDict{DynamicVar,Any}()
-getscope(task::Task) = begin
-  s = Base.get_task_tls(task)
-  b = get(s, getscope, s)
-  if b === s
-    s[getscope] = empty_scope
-  else
-    b
-  end
-end
-
-bind(var::DynamicVar, val, t::Task) = setscope(t, assoc(getscope(t), var, val))
-
-getvalue(var::DynamicVar, default, t::Task) =
-  if haskey(getscope(t), var)
-    getscope(t)[var]
-  elseif isroot(t)
-    default
-  else
-    getvalue(var, default, parent(t))
-  end
-
-# enable var[] syntax instead of need(var)
-Base.getindex(var::DynamicVar) = need(var)
--- test.jl
+++ test.jl
@@ -1,16 +1,8 @@
-include("main.jl")
-
-@dynamic a = 1
-
-@test need(a) == 1
-@dynamic let a = 2
-  @test need(a) == 2
-end
-@test need(a) == 1
+@require "." @dynamic!
 
 @dynamic! a = 2
-@test need(a) == 2
+@test a[] == 2
 @dynamic! let a = 1
-  @test need(a) == 1
+  @test a[] == 1
 end
-@test need(a) == 2
+@test a[] == 2
